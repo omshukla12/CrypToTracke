56fc6fdd4de5ea3fedcf22a019449d00473a3538
Om Shukla <121335245+omshukla12@users.noreply.github.com>
2024-09-04 17:39:46 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,20 @@ const Express = require("express");
 const dbConnection = require("./dbConnection");
 const app = Express();
 const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
 app.use(Express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({"message": "Hello friend."});
+});
+
 app.use("/api/v1", require("./router/routes"));
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   dbConnection();
-  console.log("My app is listening");
+  console.log(`My app is listening on port ${PORT}`);
 });
